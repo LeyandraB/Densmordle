807d2abc4b4fddde1c5e6437f2cde63905929cd5
damlad12 <113645970+damlad12@users.noreply.github.com>
2022-12-03 22:49:22 -0500

    Add files via upload

--- MakeRooms.cpp
+++ MakeRooms.cpp
@@ -1,80 +1,27 @@
-#include "MakeRooms.h"
+#include <fstream>
+#include "RoomInfo.h"
 
-void MakeCASRooms(RoomInfo** rooms_CAS)
-{
+using namespace std;
+
+void MakeRooms(RoomInfo** rooms_CAS){ //can also add RoomInfo** rooms_ENG etc as arguments
     char cas[] = {'C','A','S'};
-    //CAS103
     char roomnum[] = {'1', '0', '3', '\0'};
-    rooms_CAS[0] = new RoomInfo(roomnum, cas,"https://www.bu.edu/classrooms/files/2020/08/CASTsai-1.jpg");
-    
-    //CAS114
-    strcpy(roomnum, "114");
-    rooms_CAS[1] = new RoomInfo(roomnum ,'a',cas,"https://www.bu.edu/classrooms/files/2017/01/CAS114A-%E2%80%94-Picture-Front2.jpg");
-    rooms_CAS[2] = new RoomInfo(roomnum,'b',cas,"https://www.bu.edu/classrooms/files/2015/09/CAS114B-04.jpg");
-
-    //CAS116
-    strcpy(roomnum, "216");
-    rooms_CAS[3] = new RoomInfo(roomnum,cas,"https://www.bu.edu/classrooms/files/2015/09/CAS116-1.jpg");
-
-    //CAS201
-    strcpy(roomnum, "201");
-    rooms_CAS[4] = new RoomInfo(roomnum,cas,"https://www.bu.edu/classrooms/files/2017/07/CAS201-%E2%80%94-Picture-Front2.jpg");
-    
-    //CAS204
-    strcpy(roomnum, "204");
-    rooms_CAS[5] = new RoomInfo(roomnum,'a',cas,"https://www.bu.edu/classrooms/files/2013/08/CAS204A-Front-636x424.jpg");
-    rooms_CAS[6] = new RoomInfo(roomnum,'b',cas, "https://www.bu.edu/classrooms/files/2013/08/CAS204B-Front-636x424.jpg");
-    
-    //CAS208
-    strcpy(roomnum, "208");
-    rooms_CAS[7] = new RoomInfo(roomnum,cas,"https://www.bu.edu/classrooms/files/2015/03/CAS208-Front001.jpg");
-
-    //CAS211
-    strcpy(roomnum, "211");
-    rooms_CAS[8] = new RoomInfo(roomnum,cas,"https://www.bu.edu/classrooms/files/2013/08/CAS211-Front.jpg");
-    
-    //CAS212
-    strcpy(roomnum, "212");
-    rooms_CAS[9] = new RoomInfo(roomnum,cas,"https://www.bu.edu/classrooms/files/2015/09/CAS212-1.jpg");
-
-    //CAS213
-    strcpy(roomnum, "213");
-    rooms_CAS[10] = new RoomInfo(roomnum,cas,"https://www.bu.edu/classrooms/files/2017/08/CAS213-%E2%80%94-Picture-Back1.jpg");
-    
-    //CAS214
-    strcpy(roomnum, "214");
-    rooms_CAS[11] = new RoomInfo(roomnum,cas,"https://www.bu.edu/classrooms/files/2015/09/CAS214-1.jpg");
-    
-    //CAS216
-    strcpy(roomnum, "216");
-    rooms_CAS[12] = new RoomInfo(roomnum,cas,"https://www.bu.edu/classrooms/files/2017/08/CAS216-%E2%80%94-Picture-Back1.jpg");
-    
-    //CAS218
-    strcpy(roomnum, "218");
-    rooms_CAS[13] = new RoomInfo(roomnum,cas,"https://www.bu.edu/classrooms/files/2015/03/CAS218-Front001.jpg");
-    
-    //CAS220
-    strcpy(roomnum, "220");
-    rooms_CAS[14] = new RoomInfo(roomnum,cas,"https://www.bu.edu/classrooms/files/2013/08/CAS220-Front-636x424.jpg");
-    
-    //CAS221
-    strcpy(roomnum, "221");
-    rooms_CAS[15] = new RoomInfo(roomnum,cas,"https://www.bu.edu/classrooms/files/2015/09/CAS221-1.jpg");
-    
-    //CAS222
-    strcpy(roomnum, "222");
-    rooms_CAS[16] = new RoomInfo(roomnum,cas,"https://www.bu.edu/classrooms/files/2013/08/CAS222-Front-636x423.jpg");
-    
-    //CAS223
-    strcpy(roomnum, "223");
-    rooms_CAS[17] = new RoomInfo(roomnum,cas,"https://www.bu.edu/classrooms/files/2017/08/CAS223-%E2%80%94-Picture-Front1.jpg");
-    
-    //CAS224
-    strcpy(roomnum, "224");
-    rooms_CAS[18] = new RoomInfo(roomnum,cas,"https://www.bu.edu/classrooms/files/2013/08/CAS224-Front.jpg");
-    
-    //CAS203
-    strcpy(roomnum, "203");
-    rooms_CAS[19] = new RoomInfo(roomnum,cas,"https://www.bu.edu/classrooms/files/2017/07/CAS203-%E2%80%94-Picture-Front2.jpg");
-
-}
+    char digit_1;
+    char digit_2;
+    char digit_3;
+    string string_link;
+    ifstream fin;
+    fin.open("Allrooms.txt");
+    // loop to create CAS rooms
+    for (int i = 0; i < 20 ; i++){
+        while (!fin.eof()){
+            fin >>digit_1>>digit_2>>digit_3 >> string_link;
+            cout <<digit_1<<digit_2<<digit_3 << ' ' << string_link;
+            roomnum[0] = digit_1;
+            roomnum[1] = digit_2;
+            roomnum[2] = digit_3;
+            rooms_CAS[i] = new RoomInfo(roomnum,cas,string_link);
+        }}
+    fin.close();
+    //create rooms in other buildings
+    }
