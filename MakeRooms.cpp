4b1c28a566d9f2be573eb54ed541e64b6121fb63
damlad12 <113645970+damlad12@users.noreply.github.com>
2022-12-04 00:14:15 -0500

    Add files via upload

--- MakeRooms.cpp
+++ MakeRooms.cpp
@@ -3,25 +3,27 @@
 
 using namespace std;
 
-void MakeRooms(RoomInfo** rooms_CAS){ //can also add RoomInfo** rooms_ENG etc as arguments
-    char cas[] = {'C','A','S'};
+void MakeRooms(RoomInfo** rooms){ //can also add RoomInfo** rooms_ENG etc as arguments
+    char building_chars[] = {'C','A','S'};
     char roomnum[] = {'1', '0', '3', '\0'};
     char digit_1;
     char digit_2;
     char digit_3;
+    char letter_1,letter_2,letter_3;
     string string_link;
     ifstream fin;
     fin.open("Allrooms.txt");
     // loop to create CAS rooms
-    for (int i = 0; i < 20 ; i++){
         while (!fin.eof()){
-            fin >>digit_1>>digit_2>>digit_3 >> string_link;
-            cout <<digit_1<<digit_2<<digit_3 << ' ' << string_link;
+            fin >> letter_1 >> letter_2 >> letter_3 >> digit_1 >> digit_2 >> digit_3 >> string_link;
             roomnum[0] = digit_1;
             roomnum[1] = digit_2;
             roomnum[2] = digit_3;
-            rooms_CAS[i] = new RoomInfo(roomnum,cas,string_link);
-        }}
+            building_chars[0] = letter_1;
+            building_chars[1] = letter_2;
+            building_chars[2] = letter_3;
+            rooms[i] = new RoomInfo(roomnum,building_chars,string_link);
+        }
     fin.close();
     //create rooms in other buildings
     }
