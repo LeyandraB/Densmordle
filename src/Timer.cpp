a2afaedbdb94c4258999b52b2b980e272c3d1ed9
damlad12 <113645970+damlad12@users.noreply.github.com>
2022-12-09 17:19:02 -0500

    Create Timer.cpp
    compiles with -std=c++11

---
+++ src/Timer.cpp
@@ -0,0 +1,16 @@
+#include <chrono>
+#include <iostream>
+#include <future>
+
+void minute_timer() {
+    for(int i=0;i<60;i++)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
+int main ()
+{
+    auto future = std::async(minute_timer);
+    //play game here 
+    return 0;
+}
